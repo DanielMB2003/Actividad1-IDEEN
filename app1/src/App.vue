7943e6fbe67be2cf0b0518f000e88fdf0e97dcc1
Daniel Méndez Beltrán <85456831+DanielMB2003@users.noreply.github.com>
2023-03-31 12:29:58 -0600

    Add files via upload

---
+++ app1/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="#384FFE"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Logo Tec"
+          class="shrink mr-2"
+          contain
+          src="@/assets/logoTec.png"
+          transition="scale-transition"
+          width="200"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+      <v-btn
+        target="_blank"
+        text
+      >
+        <router-link style="color: white" to="/">Inicio</router-link>
+      </v-btn>
+
+      <p> </p>
+      <v-btn
+        target="_blank"
+        text
+      >
+        <router-link style="color: white" to="/about">Lista de Materias</router-link>
+      </v-btn>
+
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
